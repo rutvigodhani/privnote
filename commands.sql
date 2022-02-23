68dccce36ecca3ce71ef5ed05a8275c1b4d24ec6
rutvi <rutvi@mac>
2022-02-23 19:34:59 +0530

    initial

--- commands.sql
+++ commands.sql
@@ -1,36 +1,15 @@
-use mySchema;
-
-drop table temp;
-
-create table temp (note varchar(255), destroyWhen varchar(255), pwd varchar(255), hashValue varchar(255), createdAt timestamp);
-
-insert into temp(note, destroyWhen, pwd, hashValue, createdAt) values ("urll","yess","contentt","asd",NOW());
-
-select * from temp;
-
-select * from temp where hashValue = "asdasd";
-
-delete from temp where hashValue="Qdk86lzTIki";
-
-SHOW PROCESSLIST;
-
-SELECT NOW();
-
-select TIMESTAMPDIFF(MINUTE,NOW(),createdAt) from temp;
-
-select * from temp where TIMESTAMPDIFF(MINUTE,createdAt,NOW()) > 60 and destroyWhen = "1hour";
-
+CREATE DATABASE mySchema;
+USE mySchema;
+CREATE TABLE temp (note varchar(255), destroyWhen varchar(255), pwd varchar(255), hashValue varchar(255), createdAt timestamp);
+delimiter |
 CREATE EVENT e_24hour
     ON SCHEDULE
       EVERY 1 MINUTE
-    COMMENT 'clears table entry every 1 hour, 6 hour and 24 hour'
+    COMMENT 'clears table entry every 1 hour, 24 hour and 7 days'
     DO
       BEGIN
-	    DELETE FROM temp WHERE TIMESTAMPDIFF(MINUTE,createdAt,NOW()) > 60 and destroyWhen = "1hour";
-	    DELETE FROM temp WHERE TIMESTAMPDIFF(MINUTE,createdAt,NOW()) > 1440 and destroyWhen = "24hour";
-	    DELETE FROM temp WHERE TIMESTAMPDIFF(MINUTE,createdAt,NOW()) > 10080 and destroyWhen = "7days";
-      END
-
-drop event e_24hour;
-
-SHOW EVENTS;
+        DELETE FROM temp WHERE TIMESTAMPDIFF(MINUTE,createdAt,NOW()) > 60 and destroyWhen = "1hour";
+        DELETE FROM temp WHERE TIMESTAMPDIFF(MINUTE,createdAt,NOW()) > 1440 and destroyWhen = "24hour";
+        DELETE FROM temp WHERE TIMESTAMPDIFF(MINUTE,createdAt,NOW()) > 10080 and destroyWhen = "7days";
+      END |
+delimiter ;
--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ var connection = mysql2.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 module.exports = connection.promise();
